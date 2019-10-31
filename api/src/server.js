83f38e36053ca1cb6ff35984b7653530e496b9b6
AlexKif <sashaplakhotniy@gmail.com>
2019-10-31 15:14:44 +0200

    master: update api

--- api/src/server.js
+++ api/src/server.js
@@ -1,27 +1,51 @@
-const tasks = [{name: 'sasha', id: 1}, {name: 'wefwef', id: 2}];
 const express = require('express');
-const app = express();
-const port = 3004;
+const mysql = require('mysql');
 
-const logRequest = ({method, url}, res, next) => {
-    console.log(`[${new Date().toISOString()}] ${method} ${url}`);
-    next()
-};
+const connection = mysql.createConnection({
+    host     : 'localhost',
+    user     : 'kif',
+    password : '159753qqq'
+});
+
+connection.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected!");
+});
+
+const app = express();
+const port = 3001;
+const data = [{id: 1, name: "sasha"}, {id: 2, name: "kostya"}, {id: 3, name: "max"}];
 
 app.use(express.json());
 
-app.use(logRequest);
-
-app.get('/tasks', (req, res) => {
-    res.json(tasks)
+app.get('/', (req, res) => {
+    res.json(data)
 });
 
-app.post('/tasks', (req, res) => {
-    const task = req.body;
-    tasks.push(task);
-    res.json(task)
+app.post('/', (req, res) => {
+    const temp = req.body;
+    data.push(temp);
+    res.json(temp)
 });
 
-app.listen(port, () => {
-    console.log(`Server started`);
+app.delete('/:id', (req, res) => {
+    const id = req.params.id;
+    const findId = data.findIndex(item => item.id == id );
+    const test = data.splice(findId, 1);
+    res.status(200).json({successfully: "task deleted"});
+    res.json(test);
 });
+
+app.put('/:id', (req, res) => {
+    const id = req.params.id;
+    const task = data.find(t => t.id == id);
+    if(task) {
+        Object.assign(task, req.body);
+        res.json(task)
+    } else {
+        res.status(404).json({error: 'task not found'})
+    }
+});
+
+app.listen(port);
+connection.end();
